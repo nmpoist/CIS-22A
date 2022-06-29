e2b39a1cf7e65e681f6bdf9d41add40324c583ed
nmpoist <npoist78@gmail.com>
2022-06-28 23:19:12 -0700

    just testing that I can create a project

---
+++ ElPapaLovesNat/ElPapaLovesNat/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ElPapaLovesNat
+//
+//  Created by Natalie Poist on 6/28/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "I love you!\n";
+    return 0;
+}
